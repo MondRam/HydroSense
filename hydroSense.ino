440400793317ce863b2df5afda0ed50cdd4ef4d9
Mont Ramírez <montsesita78@gmail.com>
2023-09-12 13:03:48 -0600

    feat: comments added

--- hydroSense.ino
+++ hydroSense.ino
@@ -1,21 +1,21 @@
-#include <DHT.h>
-#include <LiquidCrystal_I2C.h>
+#include <DHT.h> //libreria sensor de temperatura
+#include <LiquidCrystal_I2C.h> //libreria pantalla 
 
 
-#define DHTPIN 2         // Pin al que está conectado el sensor DHT11
-#define DHTTYPE DHT11   
+#define DHTPIN 2  // Pin al que está conectado el sensor DHT11 (humedad)
+#define DHTTYPE DHT11  //modelo e sensor de humedad
 
-//PAntalla
+//Caracteristicas de la pantalla
 #define ANCHURA_LCD 16
 #define ALTURA_LCD 2
 #define DIRECCION_LCD 0x27
 LiquidCrystal_I2C pantalla(DIRECCION_LCD, ANCHURA_LCD, ALTURA_LCD);
 
 
-int sensorLluvia = A1;
-int sensorHumedad = A0;
-const int bomba = A3;
-int buzzer = 3;
+int sensorLluvia = A1; //pin al que está conectado el sensor de lluvia
+int sensorHumedad = A0; //pin del sensor de humedad
+const int bomba = A3; //pin de la bomba
+int buzzer = 3; //pin el buzzer
 
 
 DHT dht(DHTPIN, DHTTYPE); // Inicializar el sensor DHT
@@ -24,12 +24,13 @@ void setup() {
   Serial.begin(9600);
   dht.begin();  // Inicializar el sensor DHT
 
+//inicializar la pantalla
   pantalla.init();
   pantalla.backlight();
   pantalla.clear();
 
-  pinMode(bomba, OUTPUT);
-  pinMode(buzzer, OUTPUT);
+  pinMode(bomba, OUTPUT); //definir que la bomba es salida
+  pinMode(buzzer, OUTPUT); //definir que el buzzer es salida
 
 }
   
@@ -38,22 +39,18 @@ void loop() {
   float temperatura = dht.readTemperature();
   float humedad = dht.readHumidity();
 
+  int valorLluvia = analogRead(sensorLluvia); // Leer el valor del sensor de lluvia
+  int valorHumedad = analogRead(sensorHumedad); //Leer el valor del sensor de humedad
 
-
-  // Leer el valor del sensor de lluvia
-  int valorLluvia = analogRead(sensorLluvia);
-  int valorHumedad = analogRead(sensorHumedad);
-
-  // Imprimir los valores en el Monitor Serie
-  Serial.print("Temperatura: ");
+  // Imprimir en el monitor los valores de humead y temperatura
+  Serial.print("Temperatura °C: ");
   Serial.println(temperatura);
-  //Serial.print("°C");
-  Serial.print("Humedad: ");
+  Serial.print("Humedad %: ");
   Serial.println(humedad);
-  //Serial.print("%");
   
-  //Sensor de lluvia
-  Serial.print(valorLluvia);
+  
+  //Sernsor de lluvia: especificar los valores del sensor para que imprima texto en el monitor sobre estos valores
+  //Serial.print(valorLluvia); <-- en caso de querer imprimir el valor del sensor
   if (valorLluvia > 950) {
     Serial.println("No hay lluvia");
   } else if(valorLluvia <=950 && valorLluvia >=600) {
@@ -66,8 +63,8 @@ void loop() {
 
 
 
-//Sensor de Humedad
-  Serial.print(valorHumedad);
+//Sensor de Humedad: especificar los valores del sensor para que imprima texto en el monitor sobre estos valores
+ // Serial.print(valorHumedad); <-- en caso de querer imprimir el valor del sensor
   if (valorHumedad < 162) {
     Serial.println("Humedad de la tierra al 0-25%");
   } else if(valorLluvia >=162 && valorLluvia <325) {
@@ -79,12 +76,12 @@ void loop() {
   }
 
 //Pantalla
-  pantalla.setCursor(0, 0); // X, Y
-  pantalla.print("Temp: ");
-  pantalla.print(temperatura);
+  pantalla.setCursor(0, 0); // X, Y  posicionar el cursor en el recuadro requerido
+  pantalla.print("Temp: "); //texto a mostrar en la pantalla
+  pantalla.print(temperatura); //información mostrada en la pantalla
   pantalla.print("C ");
 
-  pantalla.setCursor(0, 1);
+  pantalla.setCursor(0, 1);  // X, Y  posicionar el cursor en el recuadro requerido
   pantalla.print("Hum: ");
   pantalla.print(humedad);
   pantalla.print("% ");
@@ -92,27 +89,22 @@ void loop() {
   
 //BOMBA
   if (valorLluvia < 950) {
-    digitalWrite(bomba, HIGH);
+    digitalWrite(bomba, HIGH); //si el sensor de lluvia detecta lluvia la bomba se apaga
   } else {
-      if (valorHumedad > 325) {
+      if (valorHumedad > 325) {// si el sensor de humedad tiene poca humedad, la bomba se prende
     digitalWrite(bomba, HIGH);
   } else {
     digitalWrite(bomba, LOW);
   }
 
-    
-    //digitalWrite(bomba, LOW);
   }
 
 //Buzzer
-  if (valorLluvia <=950 ) {
-    tone(buzzer, 10);
+  if (valorLluvia <=950 ) { //si el sensor de lluvia detecta lluvia, el buzzer lanza una alarma
+    tone(buzzer, 10); // Tono del buzzer
   } else{
-    noTone(buzzer);
+    noTone(buzzer); //sino, buzzer apagado
   }
   
-
-  // Agrega aquí la lógica para controlar actuadores o tomar decisiones basadas en los valores de los sensores
-
-  delay(1000); // Puedes ajustar el retardo según tus necesidades
+  delay(1000); // retardo de 1 segundo entre loops
 }
